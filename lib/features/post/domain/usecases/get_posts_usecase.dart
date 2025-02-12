8a200d99f5b0657701f006b76ffed55860a670d4
MaratYskak <yskakmarat2@gmail.com>
2025-02-12 06:40:58 +0500

    add get_posts_usecase

---
+++ lib/features/post/domain/usecases/get_posts_usecase.dart
@@ -0,0 +1,12 @@
+import 'package:testovoe/features/post/domain/entities/post_entity.dart';
+import 'package:testovoe/features/post/domain/repository/post_repository.dart';
+
+class GetPostsUsecase {
+  final PostRepository repository;
+
+  GetPostsUsecase({required this.repository});
+
+  Stream<List<PostEntity>> call(PostEntity post) {
+    return repository.getPosts(post);
+  }
+}
