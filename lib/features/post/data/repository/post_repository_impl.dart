fd4b0c0728a63503e7ab3a7f0ee1496f5dfd0a6b
MaratYskak <yskakmarat2@gmail.com>
2025-02-17 04:56:25 +0500

    add post_repository_impl

---
+++ lib/features/post/data/repository/post_repository_impl.dart
@@ -0,0 +1,12 @@
+import 'package:testovoe/features/post/data/remote/post_remote_data_source.dart';
+import 'package:testovoe/features/post/domain/entities/post_entity.dart';
+import 'package:testovoe/features/post/domain/repository/post_repository.dart';
+
+class PostRepositoryImpl implements PostRepository {
+  final PostRemoteDataSource remoteDataSource;
+
+  PostRepositoryImpl({required this.remoteDataSource});
+
+  @override
+  Stream<List<PostEntity>> getPosts() => remoteDataSource.getPosts();
+}
