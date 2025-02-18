c3d739c86067fa2252fae44d64d81c1800991ab6
MaratYskak <yskakmarat2@gmail.com>
2025-02-18 16:03:54 +0500

    add home_page, photos_page

---
+++ lib/features/app/home/home_page.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:testovoe/features/app/theme/style.dart';
+import 'package:testovoe/features/photo/presentation/pages/photos_page.dart';
+import 'package:testovoe/features/post/presentation/pages/posts_page.dart';
+
+class HomePage extends StatefulWidget {
+  final String uid;
+  final int? index;
+
+  const HomePage({super.key, required this.uid, this.index});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin, WidgetsBindingObserver {
+  TabController? _tabController;
+  int _currentTabIndex = 0;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    _tabController = TabController(length: 3, vsync: this);
+
+    _tabController!.addListener(() {
+      setState(() {
+        _currentTabIndex = _tabController!.index;
+      });
+    });
+
+    if (widget.index != null) {
+      setState(() {
+        _currentTabIndex = widget.index!;
+        _tabController!.animateTo(1);
+      });
+    }
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    _tabController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "WhatsApp",
+          style: TextStyle(fontSize: 20, color: greyColor, fontWeight: FontWeight.w600),
+        ),
+        actions: [
+          Row(
+            children: [
+              const Icon(
+                Icons.camera_alt_outlined,
+                color: greyColor,
+                size: 28,
+              ),
+              const SizedBox(
+                width: 25,
+              ),
+              const Icon(Icons.search, color: greyColor, size: 28),
+              const SizedBox(
+                width: 10,
+              ),
+              PopupMenuButton<String>(
+                icon: const Icon(
+                  Icons.more_vert,
+                  color: greyColor,
+                  size: 28,
+                ),
+                color: appBarColor,
+                iconSize: 28,
+                onSelected: (value) {},
+                itemBuilder: (context) => <PopupMenuEntry<String>>[
+                  PopupMenuItem<String>(
+                    value: "Settings",
+                    child: GestureDetector(
+                        onTap: () {
+                          // Navigator.pushNamed(context, PageConst.settingsPage, arguments: widget.uid);
+                        },
+                        child: const Text('Settings')),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+        bottom: TabBar(
+          labelColor: tabColor,
+          unselectedLabelColor: greyColor,
+          indicatorColor: tabColor,
+          controller: _tabController,
+          tabs: const [
+            Tab(
+              child: Text(
+                "Chats",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+              ),
+            ),
+            Tab(
+              child: Text(
+                "Status",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+              ),
+            ),
+            Tab(
+              child: Text(
+                "Calls",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          PostsPage(),
+          PhotosPage(),
+        ],
+      ),
+    );
+  }
+}
