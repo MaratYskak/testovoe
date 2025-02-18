7d1c14ad62eee84859b2c15cf87ad1b22e67dd7d
MaratYskak <yskakmarat2@gmail.com>
2025-02-18 21:30:03 +0500

    update todo_page

--- lib/features/todo/presentation/pages/todo_page.dart
+++ lib/features/todo/presentation/pages/todo_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:testovoe/features/todo/presentation/cubit/todos_cubit.dart';
 
 class TodoPage extends StatefulWidget {
   const TodoPage({super.key});
@@ -11,8 +13,67 @@ class _TodoPageState extends State<TodoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('todo page'),
+      // appBar: AppBar(
+      //   title: Text("Posts"),
+      //   centerTitle: true,
+      // ),
+      body: BlocBuilder<TodosCubit, TodosState>(
+        builder: (context, state) {
+          if (state is TodosLoaded) {
+            return ListView.builder(
+              padding: EdgeInsets.all(8.0),
+              itemCount: state.todos.length,
+              itemBuilder: (context, index) {
+                final todo = state.todos[index];
+                return Card(
+                  color: const Color.fromARGB(255, 34, 59, 102),
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          todo.title ?? "Без заголовка",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        todo.completed!
+                            ? Icon(Icons.done)
+                            : Text(
+                                'кашан бтресн??',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                ),
+                              ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          } else {
+            return Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.error_outline, size: 48, color: Colors.redAccent),
+                  SizedBox(height: 16),
+                  Text(
+                    "Ошибка загрузки постов",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            );
+          }
+        },
       ),
     );
   }
