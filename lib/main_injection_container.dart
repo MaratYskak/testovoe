624630bf08043107004fa8132b88e201a8387dd5
MaratYskak <yskakmarat2@gmail.com>
2025-02-19 06:43:26 +0500

    update main_injection_container

--- lib/main_injection_container.dart
+++ lib/main_injection_container.dart
@@ -2,6 +2,7 @@ import 'package:get_it/get_it.dart';
 import 'package:testovoe/features/photo/photos_injection_container.dart';
 import 'package:testovoe/features/post/posts_injection_container.dart';
 import 'package:testovoe/features/todo/todos_injection_container.dart';
+import 'package:testovoe/features/user/user_injection_container.dart';
 
 final sl = GetIt.instance;
 
@@ -15,4 +16,5 @@ Future<void> init() async {
   await postsInjectionContainer();
   await photosInjectionContainer();
   await todosInjectionContainer();
+  await usersInjectionContainer();
 }
